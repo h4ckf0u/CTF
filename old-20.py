cd23f0c3ce6cc5628bba65e7ab40d106698d0180
h4ckf0u <47907504+h4ckf0u@users.noreply.github.com>
2020-01-20 17:51:11 +0900

    Webhacking.kr chapcha timing attack Problem

---
+++ old-20.py
@@ -0,0 +1,5 @@
+document.lv5frm.id.value="chaser"
+document.lv5frm.cmt.value="chaser"
+var capt = document.getElementsByName("captcha_")[0].value
+document.lv5frm.captcha.value = capt
+lv5frm.submit()
