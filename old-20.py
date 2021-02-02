0eef537cb2f18d3d2f79c7910e72254e36d8bcfa
h4ckf0u <47907504+h4ckf0u@users.noreply.github.com>
2021-02-02 20:43:22 +0900

    Delete old-20.py

--- old-20.py
+++
@@ -1,5 +0,0 @@
-document.lv5frm.id.value="chaser"
-document.lv5frm.cmt.value="chaser"
-var capt = document.getElementsByName("captcha_")[0].value
-document.lv5frm.captcha.value = capt
-lv5frm.submit()
